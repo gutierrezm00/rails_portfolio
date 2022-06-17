82dd3830244bcc47af6aacfe5e199e266bc3114e
Mathew Gutierrez <mathewgutierrez00@gmail.com>
2022-06-16 22:25:39 -0600

    Initial work

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  get 'users/index'
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts
+
+  # Defines the root path route ("/")
+  root to: "home#index"
+end
