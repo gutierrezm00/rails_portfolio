2d8d4af0fecb5e7064d1127636258cd2ce83fe3a
Mathew Gutierrez <mathewgutierrez00@gmail.com>
2022-06-18 22:07:43 -0600

    work done still broke

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
-  get 'users/index'
-  get 'home/index'
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts
 
